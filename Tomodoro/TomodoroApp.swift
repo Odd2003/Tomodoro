d373760b220941b339cd950cdee6d7934393d917
Daniyar <donchik020202@gmail.com>
2025-10-16 17:54:32 +0200

    Initial Commit

---
+++ Tomodoro/TomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  TomodoroApp.swift
+//  Tomodoro
+//
+//  Created by Daniyar Yegeubay on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
